71c2ac89da2e1f461eb3b7bdcd333b4d58e609f0
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2023-12-31 00:14:58 +0700

    Modify: turn MoveResult.java and Move.java to record class

--- SourceCode/CoGanhGame/src/main/java/hust/hedspi/coganhgame/Model/Move.java
+++ SourceCode/CoGanhGame/src/main/java/hust/hedspi/coganhgame/Model/Move.java
@@ -1,25 +1,4 @@
 package hust.hedspi.coganhgame.Model;
 
-public class Move {
-    private final Piece piece;
-    private final Tile fromTile;
-    private final Tile toTile;
-
-    public Move(Piece piece, Tile fromTile, Tile toTile) {
-        this.piece = piece;
-        this.fromTile = fromTile;
-        this.toTile = toTile;
-    }
-
-    public Piece getPiece() {
-        return piece;
-    }
-
-    public Tile getFromTile() {
-        return fromTile;
-    }
-
-    public Tile getToTile() {
-        return toTile;
-    }
+public record Move(Tile fromTile, Tile toTile) {
 }
--- SourceCode/CoGanhGame/src/main/java/hust/hedspi/coganhgame/Model/MoveResult.java
+++ SourceCode/CoGanhGame/src/main/java/hust/hedspi/coganhgame/Model/MoveResult.java
@@ -2,33 +2,5 @@ package hust.hedspi.coganhgame.Model;
 
 import java.util.ArrayList;
 
-public class MoveResult {
-
-    private final boolean isValidMove;
-    private final ArrayList<Piece> capturedPieces;
-
-    public MoveResult(boolean isValidMove, ArrayList<Piece> capturedPieces) {
-        // this constructor is used when the move is a capture move
-        this.isValidMove = isValidMove;
-        this.capturedPieces = capturedPieces;
-    }
-
-    public MoveResult(boolean isValidMove) {
-        // this constructor is used when the move is a normal move
-        this.isValidMove = isValidMove;
-        this.capturedPieces = null;
-    }
-
-    public boolean isValidMove() {
-        return isValidMove;
-    }
-
-    public boolean isCaptureMove() {
-        return capturedPieces != null;
-    }
-
-    public ArrayList<Piece> getCapturedPieces() {
-        return capturedPieces;
-    }
-
+public record MoveResult(boolean isValidMove, ArrayList<Piece> capturedPieces) {
 }
